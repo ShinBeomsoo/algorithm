5bc1405ccfe3a96868f1aceed23f86d6bf1620ae
ShinBeomsoo <beomsooshin.leo@gmail.com>
2024-06-30 00:21:28 +0900

    리뷰 반영

--- divide_conquer/17829.py
+++ divide_conquer/17829.py
@@ -5,25 +5,33 @@
 # 3. 정사각형에서 가장 큰 수를 구한다.
 
 size = int(input())
-matrix = []
+matrix = []  # mat도 괜찮음.
 rectangle = []
 
 for s in range(size):
-    matrix_s = list(map(int, input().strip().split()))
-    matrix.append(matrix_s)
+    row = list(map(int, input().strip().split()))  # matrix_s -> row
+    matrix.append(row)
 
-
-while 1:
+curr_size = size
+#TODO recursive function으로 구현
+while curr_size > 1:
     new_matrix = []
-    for i in range(0, size, 2):
+    for i in range(0, curr_size, 2):
         new_row = []
-        for j in range(0, size, 2):
-            max_number = sorted([matrix[i][j], matrix[i][j+1], matrix[i+1][j], matrix[i+1][j+1]], reverse=True)[1]
-            new_row.append(max_number)
+        for j in range(0, curr_size, 2):
+            #TODO sorted가 필요할까????? 필요없을 듯
+            second_biggest_number = sorted(
+                [
+                    matrix[i][j],
+                    matrix[i][j + 1],
+                    matrix[i + 1][j],
+                    matrix[i + 1][j + 1],
+                ],
+                reverse=True,
+            )[1]
+            new_row.append(second_biggest_number)
         new_matrix.append(new_row)
     matrix = new_matrix
-    size //= 2
-    if size <= 1:
-        break
-print(new_matrix[0][0])
+    curr_size //= 2
 
+print(new_matrix[0][0])
